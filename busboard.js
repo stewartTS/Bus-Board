21e1d139f75a54f5d44283a6e89302313eaeecd2
Francesca Stocco <Francesca.Stocco@softwire.com>
2021-05-27 16:01:03 +0100

    Exercise Part2.1 New function to enter postcode and return longitude and latitude

--- busboard.js
+++ busboard.js
@@ -1,12 +1,8 @@
-//stop code = naptahnID
-//lineID, expected arrival
 
-//https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals HTTP/1.1
-//?app_key=28d983ba6f7e4db08abcab4117bafc35
 const fetch = require('node-fetch');
 const prompt = require('prompt-sync')();
 
-
+//const stopType = NaptanPublicBusCoachTram
 
 async function run() {
     let id = prompt(console.log("Please enter the stop code"));    
@@ -29,28 +25,20 @@ async function run() {
             console.log(sortedBuses.slice(4));
         })
 
-       
         
 }
-run()
-/*
 
+async function getPostcode(){
+    const postcode = prompt('Please enter your postcode: ');
 
+    const coordinated = await fetch(`https://api.postcodes.io/postcodes/${postcode}`)
+    .then(response => response.json())
+    .then(body => {
+        const longitude= body.result.longitude;
+        const latitude = body.result.latitude;
+        console.log(longitude, latitude);
+    });
+} 
 
-function toJson(response) {
-    return response.json()
-}
-
-async function run() {
-    const input = prompt("What postcode would you like to search for?  ")
-
-    const output = await fetch(``https://api.tfl.gov.uk/StopPoint/${id}/Arrivals``)
-        .then(toJson)
-        .catch(err => console.log(err))
-        .finally(_ => console.log("Done"));
-
-    console.log(output)
-}
-
-run()
-*/
+getPostcode();
+//run()
